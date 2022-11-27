dbe9d74ee507bf2266929436ef24a4db93a0223c
Brooke <brooke@gril.me>
2022-11-27 22:53:47 +0000

    Maybe fix order bug

--- src/getBinding.lua
+++ src/getBinding.lua
@@ -7,17 +7,18 @@ local AssignedBinding = require(script.Parent.AssignedBinding)
 
 local function getBinding(motor)
 	assert(motor, "Missing argument #1: motor")
-	
+
 	local isMotor = Flipper.isMotor(motor)
 	if not isMotor then
 		error("Provided value is not a motor!", 2)
 	end
 
+	local binding, setBindingValue = React.useState(motor:getValue())
+
 	if motor[AssignedBinding] then
 		return motor[AssignedBinding]
 	end
 
-	local binding, setBindingValue = React.useState(motor:getValue())
 	motor:onStep(setBindingValue)
 
 	motor[AssignedBinding] = binding
